e220f0842b3167e7688c52593891c43ea3d223be
Kushagra Tiwary <ktiwary@mit.edu>
2023-10-18 17:30:22 -0400

    optics updates

--- cambrian/renderer/optics.py
+++ cambrian/renderer/optics.py
@@ -39,18 +39,21 @@ def fft2dshift(input):
 
 class Optics():
 
-    def __init__(self, psf_kernel_size, wvls, min_depth, max_depth, depth_bins) -> None:
-        self.disc_depth_range = disc_depth_range
+    def __init__(self, psf_kernel_size, wvls, refractive_index, min_depth, max_depth, depth_bins) -> None:
+        """
+        Currently the optics component is set to fixed sizes: 
+            psf_kernel_size:  
+            wvls: np.array([610., 530., 470.]) * 1e-9
+            min_depth: 
+            max_depth : 
+            depth_bins: 
+        """
         self.psf_kernel_size = psf_kernel_size
-        # wvls=np.array([610., 530., 470.]) * 1e-9
         self.wvls = wvls
         self.depth_bins = depth_bins
         self.min_depth = min_depth
-        self.max_depth = max_depth #min_depth + depth_bins
-
-        self.N_R = 31
-        self.N_G = 27
-        self.N_B = 23  # size of the blur kernel
+        self.max_depth = max_depth # min_depth + depth_bins
+        self.refractive_index = refractive_index
 
         self.psf_kernel_size = 23 
         self.N_R = 31
@@ -60,22 +63,21 @@ class Optics():
         self.disc_depth_range = np.linspace(self.min_depth, self.max_depth, self.depth_bins, np.float32) 
         self.defocus_phase = self.generate_defocus_phase(self.disc_depth_range, self.psf_kernel_size, self.wvls)
 
-    def render(self, height_mask, rgb, depth):
+    def render(self, rgb, depth):
         """
-        height_mask: np.array() of size [23, 23]
         rgb: np.array(H, W, 3, dtype=np.uint8)
         depth: np.array((H, W), dtype=np.float) between (self.min_depth, max_depth)
         """
         rgb_torch = torch.tensor(rgb).unsqueeze(0)/255.
-        depth_torch = self.compute_disc_depths(depth, self.min_depth, self.max_depth).unsqueeze(0).permute(0,3,1,2)
-        psfs = self.generate_psf_from_height_map(height_mask, self.defocus_phase, self.wvls, idx, N_R, N_G, N_B)
+        depth_torch = self.compute_disc_depths(depth, self.disc_depth_range).unsqueeze(0).permute(0,3,1,2)
+        psfs = self.generate_psf_from_height_mask(self.height_mask, self.defocus_phase)
         rgb = self.blur_image(rgb_torch, depth_torch, psfs)
         return rgb
 
     def generate_defocus_phase(self, disc_depth_range, psf_kernel_size, wvls):
         """
-
         disc_depth_range: 
+        psf_kernel_size: 
         wvls: wavelength that psf is suceptible to. 
         """
         # return (Phi_list,pixel,pixel,color)
@@ -87,52 +89,68 @@ class Optics():
             for k in range(len(wvls)):
                 defocus_phase[j, :, :, k] = phi * (xx ** 2 + yy ** 2) * wvls[1] / wvls[k];
         return defocus_phase
+    
+    def _load_zernike_vars(self, path):
+        # uncomment to load zernekie polynomials and height mask from scratch!
+        zernike = sio.loadmat(path) # 'tools/zernike_basis.mat'
+        self.u2 = zernike['u2']  # basis of zernike poly
+        self.n_coeff_zernike = self.u2.shape[1]
+        idx = zernike['idx']
+        self.idx = idx.astype(np.float32)
 
-    def get_height_map(self, alpha_zernike, n_coeff_zernike, u2, psf_kernel_size, wvls, init_func='random'):
+    def get_height_mask_from_zernike_vars(self, path, init_func='random'):
 
-        if alpha_zernike == None:
-            alpha_zernike = torch.zeros((n_coeff_zernike, 1), dtype=torch.float32)
+        self._load_zernike_vars(path)
 
-        clip_alphas = lambda x: torch.clip(x, -wvls[1] / 2, wvls[1] / 2)
+        if init_func == 'zeros':
+            alpha_zernike = torch.zeros((self.n_coeff_zernike, 1), dtype=torch.float32)
+        elif init_func == 'random':
+            alpha_zernike = torch.rand((self.n_coeff_zernike, 1), dtype=torch.float32)
+        else:
+            raise ValueError("{} not found".format(init_func))
+
+        clip_alphas = lambda x: torch.clip(x, - self.wvls[1] / 2, self.wvls[1] / 2)
         alpha_zernike = clip_alphas(alpha_zernike)
-        g = torch.matmul(torch.tensor(u2), alpha_zernike)
-        height_map = torch.relu(g.reshape((psf_kernel_size, psf_kernel_size)) + wvls[1])
-        return height_map
+        g = torch.matmul(torch.tensor(self.u2), alpha_zernike)
+        self.height_mask = torch.relu(g.reshape((self.psf_kernel_size, self.psf_kernel_size)) + self.wvls[1])
+        
+    def load_height_mask_from_file(self, zernike_path, height_mask_path): 
+        self._load_zernike_vars(zernike_path)
+        self.height_mask = np.loadtxt(height_mask_path).astype(np.float32)
 
-    def generate_psf_from_height_map(self, height_mask, defocus_phase, wvls, idx, N_R, N_G, N_B, refactive_idx=1.5):
-        idx = torch.tensor(idx)
+    def generate_psf_from_height_mask(self, height_mask, defocus_phase):
+        idx = torch.tensor(self.idx)
         height_mask = torch.tensor(height_mask)
         defocus_phase = torch.tensor(defocus_phase)
         
         defocus_phase_r = defocus_phase[:, :, :, 0]
-        phase_R = torch.add(2 * np.pi / wvls[0] * (refactive_idx - 1) * height_mask, defocus_phase_r)
+        phase_R = torch.add(2 * np.pi / self.wvls[0] * (self.refractive_index - 1) * height_mask, defocus_phase_r)
         e_defocused_r = torch.mul(torch.complex(idx, torch.tensor(0.0)), torch.exp(torch.complex(torch.tensor(0.0), phase_R)))
 
-        pad_r = ((N_R - N_B) // 2, (N_R - N_B) // 2, (N_R - N_B) // 2, (N_R - N_B) // 2)
+        pad_r = ((self.N_R - self.N_B) // 2, (self.N_R - self.N_B) // 2, (self.N_R - self.N_B) // 2, (self.N_R - self.N_B) // 2)
         pupil_r = torch.nn.functional.pad(e_defocused_r, pad_r)
-        norm_r = N_R * N_R * torch.sum(idx ** 2)
+        norm_r = self.N_R * self.N_R * torch.sum(idx ** 2)
         fft_pupil_r = torch.fft.fft2(pupil_r); 
         psf_r = torch.divide(torch.square(torch.abs(fft2dshift(fft_pupil_r))), norm_r)
 
         defocus_phase_g = defocus_phase[:, :, :, 1]
-        phase_G = torch.add(2 * np.pi / wvls[1] * (refactive_idx - 1) * height_mask, defocus_phase_g)
+        phase_G = torch.add(2 * np.pi / self.wvls[1] * (self.refractive_index - 1) * height_mask, defocus_phase_g)
         e_defocused_g = torch.mul(torch.complex(idx, torch.tensor(0.0)), torch.exp(torch.complex(torch.tensor(0.0), phase_G)))
-        pad_g = ((N_G - N_B) // 2, (N_G - N_B) // 2, (N_G - N_B) // 2, (N_G - N_B) // 2)
+        pad_g = ((self.N_G - self.N_B) // 2, (self.N_G - self.N_B) // 2, (self.N_G - self.N_B) // 2, (self.N_G - self.N_B) // 2)
         pupil_g = torch.nn.functional.pad(e_defocused_g, pad_g)
-        norm_g = N_G * N_G * torch.sum(idx ** 2)
+        norm_g = self.N_G * self.N_G * torch.sum(idx ** 2)
         fft_pupil_g = torch.fft.fft2(pupil_g)
         psf_g = torch.divide(torch.square(torch.abs(fft2dshift(fft_pupil_g))), norm_g)
 
         defocus_phase_b = defocus_phase[:, :, :, 2]
-        phase_B = torch.add(2 * np.pi / wvls[2] * (refactive_idx - 1) * height_mask, defocus_phase_b)
+        phase_B = torch.add(2 * np.pi / self.wvls[2] * (self.refractive_index - 1) * height_mask, defocus_phase_b)
         pupil_b = torch.mul(torch.complex(idx, torch.tensor(0.0)), torch.exp(torch.complex(torch.tensor(0.0), phase_B)))
-        norm_b = N_B * N_B * torch.sum(idx ** 2)
+        norm_b = self.N_B * self.N_B * torch.sum(idx ** 2)
         fft_pupil_b = torch.fft.fft2(pupil_b)
         psf_b = torch.divide(torch.square(torch.abs(fft2dshift(fft_pupil_b))), norm_b)
-        print(psf_r.shape, psf_g.shape, psf_b.shape)
 
-        N_crop_R = int((N_R - N_B) / 2)  # Num of pixel need to cropped at each side for R
-        N_crop_G = int((N_G - N_B) / 2)  # Num of pixel need to cropped at each side for G
+        N_crop_R = int((self.N_R - self.N_B) / 2)  # Num of pixel need to cropped at each side for R
+        N_crop_G = int((self.N_G - self.N_B) / 2)  # Num of pixel need to cropped at each side for G
 
         psfs = torch.stack(
             [psf_r[:, N_crop_R:-N_crop_R, N_crop_R:-N_crop_R], psf_g[:, N_crop_G:-N_crop_G, N_crop_G:-N_crop_G], psf_b], axis=3)
@@ -166,25 +184,29 @@ class Optics():
             blur = normalize(blur) 
         return blur
 
-    def compute_disc_depths(self, mj_depth):
+    def compute_disc_depths(self, mj_depth, disc_depth_range):
         """
         mj_depth must be between (min, max)
         """
         disc_depth = []
-        # for each depth value see if it's close to self.disc_depth_range[n-1] < x < self.disc_depth_range[n]
-        mj_depth = mj_depth.round().astype(np.uint8)
-        # self.min_depth
-        # self.max_depth
-        for i in range(len(self.disc_depth_range)-1):
-            _min = self.disc_depth_range[i] 
-            _max = self.disc_depth_range[i+1] 
-            idx = np.where(mj_depth >= _min and mj_depth < _max)
+        
+        # append the first one 
+        idx = np.where(mj_depth < disc_depth_range[0])
+        disc_depth_i = np.zeros_like(mj_depth ,dtype=np.float32)
+        disc_depth_i[idx] = 1
+        disc_depth.append(torch.tensor(disc_depth_i).unsqueeze(-1))
+        
+        # for each depth value see if it's close to disc_depth_range[n] < x < disc_depth_range[n+1]
+        for i in range(1, len(disc_depth_range)-1):
+            _min = disc_depth_range[i] 
+            _max = disc_depth_range[i+1]
+            idx = np.where((mj_depth >= _min) & (mj_depth < _max))
             disc_depth_i = np.zeros_like(mj_depth ,dtype=np.float32)
             disc_depth_i[idx] = 1
             disc_depth.append(torch.tensor(disc_depth_i).unsqueeze(-1))
 
         # append the last one 
-        idx = np.where(mj_depth >= _min)
+        idx = np.where(mj_depth >= disc_depth_range[-1])
         disc_depth_i = np.zeros_like(mj_depth ,dtype=np.float32)
         disc_depth_i[idx] = 1
         disc_depth.append(torch.tensor(disc_depth_i).unsqueeze(-1))
@@ -199,31 +221,26 @@ if __name__ == "__main__":
     depth_path = sys.argv[2]
     rgb = np.array(Image.open(rgb_path))
     depth = np.array(np.load(depth_path))
-    print("depth (min, max): ({},{})".format(depth.min(), depth.max()))
 
     # config
-    DEPTH_BINS = 21
-    DEPTH_MIN = -10
-    DEPTH_MAX = 10
+    DEPTH_BINS = 5
+    DEPTH_MIN = 0
+    DEPTH_MAX = 1
     N_R = 31
     N_G = 27
     N_B = 23  # size of the blur kernel
-    wvls=np.array([610., 530., 470.]) * 1e-9
+    WLVS = np.array([610., 530., 470.]) * 1e-9
+    PSF_KERNEL_SIZE = 23 # we should probably 10 to keep the parameters small
+    REFRACTIVE_INDEX = 1.5
+    ZERNIKE_PATH = "./tools/zernike_basis.mat"
+    HEIGHT_MASK_PATH = "tools/FisherMask_HeightMap.txt"
 
-    psf_kernel_size = 23 # we should probably 10 to keep the parameters small
-
-    # uncomment to load zernekie polynomials and height mask from scratch!
-    zernike = sio.loadmat('tools/zernike_basis.mat')
-    u2 = zernike['u2']  # basis of zernike poly
-    n_coeff_zernike = u2.shape[1]
-    idx = zernike['idx']
-    idx = idx.astype(np.float32)
     # alpha_zernike = None
-    # height_mask = get_height_map(alpha_zernike, n_coeff_zernike, u2, psf_kernel_size, wvls)
-    height_mask_fisher = np.loadtxt('tools/FisherMask_HeightMap.txt').astype(np.float32)
 
-    optics = Optics(psf_kernel_size, wvls, DEPTH_MIN, DEPTH_MAX, DEPTH_BINS)
-    img = optics.render(height_mask_fisher, rgb, depth)
+    optics = Optics(PSF_KERNEL_SIZE, WLVS, REFRACTIVE_INDEX, DEPTH_MIN, DEPTH_MAX, DEPTH_BINS)
+    # height_mask = optics.get_height_map_from_zernike_vars(alpha_zernike, n_coeff_zernike, u2, psf_kernel_size, wvls)
+    optics.load_height_mask_from_file(ZERNIKE_PATH, HEIGHT_MASK_PATH)
+    img = optics.render(rgb, depth)
 
     fig = plt.figure(figsize=(10,10))
     ax1 = fig.add_subplot(2,2,1)
@@ -233,7 +250,7 @@ if __name__ == "__main__":
     ax2.imshow(depth)
     ax2.set_title("Sampled Depth")
     ax3 = fig.add_subplot(2,2,3)
-    ax3.imshow(height_mask_fisher)
+    ax3.imshow(optics.height_mask)
     ax3.set_title("Fisher Height Mask")
     ax4 = fig.add_subplot(2,2,4)
     ax4.imshow(img)
