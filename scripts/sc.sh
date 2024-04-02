77d9a01ecc85e14a593fc2c2f9d0e9c454165d49
Aaron Young <aryoung@mit.edu>
2024-04-01 21:44:50 -0400

    Fixed sc script

--- scripts/sc.sh
+++ scripts/sc.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=4-00:00:00
+#SBATCH --qos=high
+#SBATCH --job-name mj_training
+#SBATCH --output=out/R-%x.%j_%a.out
+#SBATCH --error=out/R-%x.%j_%a.err
 
 [ $# -eq 0 ] && (echo "Please provide the script" && return 0)
 SCRIPT=$1
 shift
 
 cmd="MUJOCO_GL=egl bash $SCRIPT hydra/launcher=supercloud $@"
->&2 echo "Running command: $cmd"
+echo "Running command: $cmd" | tee /dev/stderr
 eval $cmd
