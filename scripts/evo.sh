122c01e8f8c3f0130c7623a6f2d2da5de5fcd159
AaronYoung5 <aryoung@mit.edu>
2024-04-02 07:05:28 -0400

    Forgot to delete the evo.sh script

--- scripts/evo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-MUJOCO_GL=egl python cambrian/ml/evo.py $@ \
-    evo.num_nodes=${SLURM_ARRAY_TASK_COUNT:-1} \
-    evo.generation.rank=${SLURM_ARRAY_TASK_ID:-0} \
-    evo.generation.generation=0
