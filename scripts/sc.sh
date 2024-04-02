38094d044f55bebcbac2a1dd1eb18398d178c3a7
AaronYoung5 <aryoung@mit.edu>
2024-04-01 21:16:48 -0400

    Added runner scripts

--- scripts/sc.sh
+++ scripts/sc.sh
@@ -1,24 +1,9 @@
 #!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --cpus-per-task=40
-#SBATCH --ntasks-per-node=1
-#SBATCH --gres=gpu:volta:1
-#SBATCH --time=4-04:00:00
-#SBATCH --qos=high
-#SBATCH --job-name mj_training
-#SBATCH --output=out/R-%x.%j_%a.out
-#SBATCH --error=out/R-%x.%j_%a.err
-
-source /etc/profile
 
 [ $# -eq 0 ] && (echo "Please provide the script" && return 0)
 SCRIPT=$1
-
-export TF_CPP_MIN_LOG_LEVEL=2
-export OPENBLAS_NUM_THREADS=1
-export PMIX_MCA_gds=hash
-
 shift
 
->&2 echo "Running script $PWD..."
-MUJOCO_GL=egl bash $SCRIPT "$@"
+cmd="MUJOCO_GL=egl bash $SCRIPT hydra/launcher=supercloud $@"
+>&2 echo "Running command: $cmd"
+eval $cmd
