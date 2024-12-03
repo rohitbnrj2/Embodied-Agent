d4e46db0fd22c7183e597840d17d5c4540007520
AaronYoung5 <aryoung@mit.edu>
2024-12-03 10:25:26 -0500

    Added slurm.sh

---
+++ scripts/slurm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=4-00:00:00
+#SBATCH --job-name cambrian
+#SBATCH --output=out/R-%x.%j_%a.out
+#SBATCH --error=out/R-%x.%j_%a.err
+
+[ $# -eq 0 ] && (echo "Please provide the script" && return 0)
+SCRIPT=$1
+shift
+
+cmd="MUJOCO_GL=egl bash $SCRIPT $@"
+echo "Running command: $cmd" | tee /dev/stderr
+eval $cmd
